70fad4eae3b4e57e7cb359404eed50a498e325e7
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-21 10:14:09 -0700

    create new nested for function to accomplish 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,7 +8,12 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
 
 	for (i = 0 ; i < 10 ; i++)
-		print_alphabet();
+	{
+		for (j = 97 ; j < 123 ; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
 }
