c548758c6943f078097b737e860508ce458d6e82
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-23 07:34:55 -0700

    leap year problem fix

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -21,7 +21,7 @@ void print_remaining_days(int month, int day, int year)
 		printf("Day of the year: %d\n", day);
 		printf("Remaining days: %d\n", 366 - day);
 	}
-	else
+	else if (( year % 400 == 0) && (year % 100 == 0))
 	{
 		if (month == 2 && day == 60)
 		{
@@ -32,5 +32,10 @@ void print_remaining_days(int month, int day, int year)
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 365 - day);
 		}
+	}
+	else
+	{
+		 printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+	
 	}
 }
