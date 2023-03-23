9042cd05850b109d58022bf253bce7d42db2f7fd
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-23 16:02:23 -0700

    nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_diagonal - entry point
+ *
+ * Description: Prints diagonal line
+ *@n: number of times \ is printed
+ */
+
+void print_diagonal(int n)
+{
+	int num_lines;
+	int num_spaces;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{num_linesnum_lines
+		for (num_lines = 0; num_lines < n; num_lines++)
+		{
+			for (num_spaces = 0; num_spaces < num_lines; num_spaces++)
+				_putchar(' ');
+			_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
