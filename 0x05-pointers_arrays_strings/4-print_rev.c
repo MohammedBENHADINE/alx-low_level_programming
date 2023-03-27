5ce92d44e50f2ec720ded7be23d5e211a1ed23e6
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-27 08:32:20 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,28 +1,26 @@
 #include "main.h"
 /**
  * print_rev - print a string in backwards.
- * @s: pointer to string.
+ * @str: pointer to string.
  */
 
 void print_rev(char *str)
 {
 	char *z = str;
 	int i;
-	int count1 = 0;
 	int count = 0;
 
 	while (*str != '\0')
 	{
 		str++;
-		count1++;
+		count++;
 	}
 
-	i = count1 - 1;
+	i = count - 1;
 	while (i >= 0)
 	{
 		_putchar(*(z + i));
 		i--;
-		count++;
 	}
 	_putchar('\n');
 }
