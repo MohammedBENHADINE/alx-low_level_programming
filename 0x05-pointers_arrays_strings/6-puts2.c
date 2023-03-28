478258d4864c431cb8b7084912f4ef55f2c94a74
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-27 17:43:34 -0700

    print other characters in a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print other characters in a string.
+ * @s: pointer to string.
+ */
+
+void puts2(char *s)
+{
+	char *p;
+
+	p = s;
+	while (*p != '\0')
+	{
+		_putchar(*p);
+		p = p + 2;
+	}
+	_putchar('\n');
+}
