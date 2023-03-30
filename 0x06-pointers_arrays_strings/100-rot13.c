b41e1d3fbdfce26335ea7dc4576de8b6702fa63c
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-30 16:50:22 -0700

    fix error

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,15 +10,15 @@ char *rot13(char *s)
 {
 	char *r = s;
 	char a[] = { 'A', 'M', 'N', 'Z'};
-	char n[] = { 13, 13, -13};
-	int i = 0;
+	int n[] = { 13, 13, -13};
+	int i;
 
 	while (*s)
 	{
 		for (i = 0; i < 4; i = i + 2)
 		{
-			if (*s >= a[i] && *s <= a[i + 1] || *s >=
-					a[i] - 32 && *s <= a[i + 1] - 32)
+			if ((*s >= a[i] && *s <= a[i + 1]) || (*s >=
+					a[i] + 32 && *s <= a[i + 1] + 32))
 				*s += n[i];
 		}
 		s++;
