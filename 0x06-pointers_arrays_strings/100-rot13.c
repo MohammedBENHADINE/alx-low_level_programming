a6656ddfde0a316cca941eafdb4293d0a8bac0e1
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-30 16:41:47 -0700

    add rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encoder
+ * @s: pointer to string
+ *
+ * Description: encodes a string into rot13.
+ * Return: pointer to encoded string
+*/
+char *rot13(char *s)
+{
+	char *r = s;
+	char a[] = { 'A', 'M', 'N', 'Z'};
+	char n[] = { 13, 13, -13};
+	int i = 0;
+
+	while (*s)
+	{
+		for (i = 0; i < 4; i = i + 2)
+		{
+			if (*s >= a[i] && *s <= a[i + 1] || *s >=
+					a[i] - 32 && *s <= a[i + 1] - 32)
+				*s += n[i];
+		}
+		s++;
+	}
+	return (r);
+}
