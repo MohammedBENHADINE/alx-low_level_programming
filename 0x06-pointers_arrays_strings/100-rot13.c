87143440afac567548081f246938ca2b30c90c9c
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-03-30 16:59:01 -0700

    fix issue

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -10,7 +10,7 @@ char *rot13(char *s)
 {
 	char *r = s;
 	char a[] = { 'A', 'M', 'N', 'Z'};
-	int n[] = { 13, 13, -13};
+	int n[] = { 13, 13, -13, -13};
 	int i;
 
 	while (*s)
@@ -18,8 +18,11 @@ char *rot13(char *s)
 		for (i = 0; i < 4; i = i + 2)
 		{
 			if ((*s >= a[i] && *s <= a[i + 1]) || (*s >=
-					a[i] + 32 && *s <= a[i + 1] + 32))
+					(a[i] + 32) && *s <= (a[i + 1] + 32)))
+			{
 				*s += n[i];
+				break;
+			}
 		}
 		s++;
 	}
