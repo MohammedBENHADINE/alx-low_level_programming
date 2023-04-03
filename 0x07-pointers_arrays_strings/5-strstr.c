05b746fc29057f73e9b46a555d819e9bfa9e653f
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-03 05:17:24 -0700

    add strstr-like

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strstr - locates strnig in string
+ * @haystack: pointer to string
+ * @needle: pointer to string
+ *
+ * Description: finds the first occurrence of the substring
+ * needle in the string haystack
+ * Return: pointer to first substring
+ **/
+char *_strstr(char *haystack, char *needle)
+{
+	char *p = haystack;
+	char *q;
+	char *found = NULL;
+
+	while (*p != '\0')
+	{
+		if (*p == *needle)
+		{
+			found = p;
+			q = needle;
+			while (*p == *q && *q != '\0')
+			{
+				p++;
+				q++;
+			}
+			if (*q == '\0')
+				break;
+		}
+		else
+		{
+			p++;
+		}
+	}
+	return (found);
+}
