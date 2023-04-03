db9209e46b455dfdb4f62899283a1d976742bdf4
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-03 05:44:36 -0700

    fix limit

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stddef.h>
-#include <stdio.h>
 /**
  * _strstr - locates strnig in string
  * @haystack: pointer to string
@@ -18,18 +17,15 @@ char *_strstr(char *haystack, char *needle)
 
 	while (*p != '\0')
 	{
-		printf("at %s\n", p);
 		if (*p == *needle)
 		{
 			found = p;
 			q = needle;
-			printf("found at %s\n", found);
 			while (*p == *q && *q != '\0' && *p != '\0')
 			{
 				p++;
 				q++;
 			}
-			printf("breaking while , value of q is %s , value of q is %s \n", q, p);
 			if (*q != '\0')
 				found = NULL;
 			else
