299a035554ae17243c9ed8db15b7fc84c8d38cfd
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-18 07:33:39 -0700

    dog strict

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - new struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog's owner
+ **/
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog;
+
+#endif
