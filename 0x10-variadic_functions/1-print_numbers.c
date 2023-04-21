dca90b91b81fbb18b2b5cf28309c20351e3d2b7d
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-20 18:32:33 -0700

    add another variadic function

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+#include <stdio.h>
+/**
+ * print_numbers - another variadic func
+ * @separator: const char pointer
+ * @n: number of args
+ **/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	if (n == 0)
+		return;
+	va_start(ap, n);
+	for (i = 0; i < n; i++)
+		if (separator)
+			printf("%d%s", va_arg(ap, int), separator);
+		else
+			printf("%d", va_arg(ap, int));
+	va_end(ap);
+	printf("\n");
+}
