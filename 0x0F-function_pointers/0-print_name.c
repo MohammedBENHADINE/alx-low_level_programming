4696eb2bfb3ecd246af1ea3c9e6c5fe5968e95f4
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-21 05:20:37 -0700

    call a func by pointer

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - print name
+ * @name: name
+ * @f: func pointer to  printer
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
