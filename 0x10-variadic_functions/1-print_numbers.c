824fab559a59159814d8f26f13cd7227830a83d7
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-04-20 18:35:11 -0700

    add minor change

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		return;
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
-		if (separator)
+		if (separator != NULL)
 			printf("%d%s", va_arg(ap, int), separator);
 		else
 			printf("%d", va_arg(ap, int));
