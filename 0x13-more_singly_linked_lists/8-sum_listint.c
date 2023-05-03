325b63c0c62f4add692dc7a5dcbefef2025b3e13
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-05-03 07:54:24 -0700

    sum list elements

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * sum_listint - get sum of list
+ * @head: pointer to list
+ * Return: pointer to node or NULL
+ **/
+int sum_listint(listint_t *head);
+{
+	listint_t *tmp;
+	unsigned int count = 0;
+
+	tmp = head;
+	while (tmp)
+	{
+		count += tmp->n;
+		tmp = tmp->next;
+	}
+	return (count);
+}
