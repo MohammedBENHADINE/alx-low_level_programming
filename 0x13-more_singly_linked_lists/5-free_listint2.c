1cbed4fd8be5be826cf7c124017d4be688c08ae4
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-05-03 06:41:48 -0700

    free and set NULL

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint - free a list
+ * @head: pointer to list
+ **/
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
+	*head = NULL;
+}
