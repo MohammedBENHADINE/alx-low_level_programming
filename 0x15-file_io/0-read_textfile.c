7e7c7869b90248d10df4d1aced4dd456c8e79120
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-05-09 07:23:53 -0700

    clean code

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,24 +11,31 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fp;
-	ssize_t count = 0;
+	ssize_t wcount, rcount = 0;
 	char *buf;
 
-	if (filename == NULL)
+	if (filename == NULL || letters == 0)
 		return (0);
 	fp = open(filename, O_RDONLY);
 	if (fp < 0)
 		return (0);
 	buf = malloc(sizeof(char) * letters);
-	count = read(fp, buf, letters);
+	if (buf == NULL)
+		return (0);
+	rcount = read(fp, buf, letters);
 	if (count < 0)
 	{
 		free(buf);
 		close(fp);
 		return (0);
 	}
-	printf("%s\n", buf);
+	wcount = write(STDOUT_FILENO, buf, rcount);
+	if (wcount <= 0)
+	{
+		close(fp);
+		free(buf);
+	}
 	close(fp);
 	free(buf);
-	return (count);
+	return (wcount);
 }
