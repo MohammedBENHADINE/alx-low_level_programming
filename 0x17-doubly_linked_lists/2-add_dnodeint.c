034079d433b3ae5dcaef142ea824f1f9ee02d854
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-16 15:02:37 -0700

    add node adder

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * add_dnodeint -  add node to header
+ * @head: head of list
+ * @n: number to add
+ * Return: new head or NULL if failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->prev = NULL;
+	new->next = *head;
+	new->n = n;
+	*head = new;
+	(*head)->prev = new;
+	return (new);
+}
