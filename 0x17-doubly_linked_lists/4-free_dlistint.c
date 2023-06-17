e77accee8b3518bc6c701d55c8a566bf12e3f290
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-16 17:58:38 -0700

    free a list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_dlistint - free a list
+ * @head: pointer on head
+ * Return: nothing
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *cur;
+	
+	cur = head;
+	while (head)
+	{
+		cur = head;
+		head = head->next;
+		free(cur);
+	}
+}
