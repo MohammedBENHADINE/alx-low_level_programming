df0fc7ed97c93761264132de39ee0fc4c3c322be
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-23 08:37:41 -0700

    fix web get url

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/MohammedBENHADINE/alx-low_level_programming/tree/main/0x18-dynamic_libraries/hook.so
+wget -P /tmp https://github.com/MohammedBENHADINE/alx-low_level_programming/raw/main/0x18-dynamic_libraries/hook.so
 export LD_PRELOAD=/tmp/hook.so
