df516c00de33947e2ebdae63998d276d90a55d1f
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-06-23 03:37:59 -0700

    fix betty

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 int _putchar(char c);
@@ -20,3 +23,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif
