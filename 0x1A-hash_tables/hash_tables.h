14e9856cc6ffeb208a2ad192ba20e56b3c37f025
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-12 06:30:22 -0700

    fix betty

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -1,3 +1,5 @@
+#ifndef HASH_TABLES
+#define HASH_TABLES
 #include <stdio.h>
 #include <stdlib.h>
 /**
@@ -10,9 +12,9 @@
  */
 typedef struct hash_node_s
 {
-     char *key;
-     char *value;
-     struct hash_node_s *next;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
 } hash_node_t;
 
 /**
@@ -25,8 +27,9 @@ typedef struct hash_node_s
  */
 typedef struct hash_table_s
 {
-     unsigned long int size;
-     hash_node_t **array;
+	unsigned long int size;
+	hash_node_t **array;
 } hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
+#endif
