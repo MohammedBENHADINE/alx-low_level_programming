ce23d9c2e501ccfc5e2924c5592b221dd3882bbc
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-07-13 04:30:09 -0700

    free mem in case of error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,6 +10,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newNode;
 	unsigned long int idx;
+	hash_node_t *cur;
 
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
@@ -17,9 +18,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (newNode == NULL)
 		return (0);
 	newNode->key = (char *) malloc(strlen(key) + 1);
-	newNode->value = (char *) malloc(strlen(value) + 1);
-	if (newNode->key == NULL ||  newNode->value == NULL)
+	if (newNode->key == NULL)
+	{
+		free(newNode);
 		return (0);
+	}
+	newNode->value = (char *) malloc(strlen(value) + 1);
+	if (newNode->value == NULL)
+	{
+		free(newNode->key);
+		free(newNode);
+		return (0);
+	}
 	strcpy(newNode->key, key);
 	strcpy(newNode->value, value);
 	idx = key_index((const unsigned char *)key, ht->size);
@@ -30,6 +40,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		cur = ht->array[idx];
+		while (cur)
+		{
+			if (strcmp(cur->key, key) == 0)
+			{
+				free(cur->value);
+				cur->value = (char *) malloc(strlen(value) + 1);
+				strcpy(cur->value, value);
+				free(newNode->key);
+				free(newNode->value);
+				free(newNode);
+				return (1);
+			}
+			cur = cur->next;
+		}
 		newNode->next = ht->array[idx];
 		ht->array[idx] = newNode;
 	}
