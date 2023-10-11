9664fb06d48ea6ba85d53ba54da387dd9f2091d6
MohammedBENHADINE <mohammedbenhadine@gmail.com>
2023-10-11 14:21:33 -0700

    add jump search

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,4 +7,6 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int binary_helper(int *array, int min, int max, int value);
+int jump_search(int *array, size_t size, int value);
+
 #endif
